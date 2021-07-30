95a78649b8e202d5b2ffe7fef1b67df14b171c7e
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-07-30 14:23:06 -0400

    Add files via upload

---
+++ main (6).js
@@ -0,0 +1,43 @@
+var pin1 = createSprite(200, 300, 20, 20);
+var pin2 = createSprite(180, 320, 20, 20);
+var pin3 = createSprite(220, 320, 20, 20);
+var pin4 = createSprite(160, 340, 20, 20);
+var pin5 = createSprite(200, 340, 20, 20);
+var pin6 = createSprite(240, 340, 20, 20);
+var squariad = createSprite(200, 80, 20, 20);
+squariad.velocityY = 0.5;
+squariad.velocityX = 4;
+squariad.shapeColor = "white";
+function draw() {
+background("lightblue");
+drawSprites();
+createEdgeSprites()
+pin1.bounce(pin2);
+pin1.bounce(pin3);
+pin1.bounce(pin4);
+pin1.bounce(pin5);
+pin1.bounce(pin6);
+pin2.bounce(pin3);
+pin2.bounce(pin4);
+pin2.bounce(pin5);
+pin2.bounce(pin6);
+pin3.bounce(pin4);
+pin3.bounce(pin5);
+pin3.bounce(pin6);
+pin4.bounce(pin5);
+pin4.bounce(pin6);
+pin5.bounce(pin6);
+squariad.bounce(pin1);
+squariad.bounce(pin2);
+squariad.bounce(pin3);
+squariad.bounce(pin4);
+squariad.bounce(pin5);
+squariad.bounce(pin6);
+pin1.bounceOff(edges);
+pin2.bounceOff(edges);
+pin3.bounceOff(edges);
+pin4.bounceOff(edges);
+pin5.bounceOff(edges);
+pin6.bounceOff(edges);
+squariad.bounceOff(edges);
+}
